83712d8d51fdeae90f50dc7631e3a44bc181f801
Alexander Leali <aleali@bgsu.edu>
2024-06-25 13:15:07 -0400

    changed the hardcoded timezones into the struct

--- code/timeCalc.cpp
+++ code/timeCalc.cpp
@@ -3,30 +3,31 @@
 #include <iostream>
 #include <string>
 
-struct timeZone{
-    int hoursOffset;
+struct timeZone {
     std::string zoneName;
+    int hoursOffset;
+
 };
 
 int main(){
 
     //time zones
-    std::string timeZones[15][2] = {
-        {"PDT", "-07"},
-        {"MDT", "-06"},
-        {"CDT", "-05"},
-        {"EDT", "-04"},
-        {"BRT", "-03"},
-        {"UTC", "00"},
-        {"BST", "+01"},
-        {"CEST", "+02"},
-        {"MSK", "+03"},
-        {"AST", "+04"},
-        {"WAST", "+05"},
-        {"MPST","+08"},
-        {"JST","+09"},
-        {"AEST","+10"},
-        {"NZST","+12"},
+    timeZone timeZones[15]= {
+        {"PDT", -7},
+        {"MDT", -6},
+        {"CDT", -5},
+        {"EDT", -4},
+        {"BRT", -3},
+        {"UTC", 0},
+        {"BST", 1},
+        {"CEST", 2},
+        {"MSK", 3},
+        {"AST", 4},
+        {"WAST", 5},
+        {"MPST",8},
+        {"JST",9},
+        {"AEST",10},
+        {"NZST",12},
     };
 
     std::string userFirstTimeZone = "";
